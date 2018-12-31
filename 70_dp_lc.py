0e9db7215ee63c9d988b2e6133ec61218b941991
Ran <1012869439@qq.com>
2019-01-01 00:09:40 +0800

    @functools.lru_cache

---
+++ 70_dp_lc.py
@@ -0,0 +1,16 @@
+import functools
+
+class Solution:
+    @functools.lru_cache(maxsize=100)
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        else:
+            return self.climbStairs(n-1) + self.climbStairs(n-2)
+        
