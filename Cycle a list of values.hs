e7ba78c2f81d3a05a3ae4d4b9ffd115bd75b561b
Ran <1012869439@qq.com>
2019-01-08 14:49:18 +0800

    Ahhhh! Monad!

---
+++ Cycle a list of values.hs
@@ -0,0 +1,30 @@
+-- 1
+module Cycle where
+
+import Data.List (elemIndex)
+
+data Direction = L | R deriving (Show)
+
+cycleList :: (Eq a) => Direction -> [a] -> a -> Maybe a
+cycleList L xs x = 
+        case x `elemIndex` xs of
+            Just n  -> Just ((xs ++ xs) !! (n - 1 + (length xs)))
+            Nothing -> Nothing
+cycleList R xs x = 
+        case x `elemIndex` xs of
+            Just n  -> Just ((xs ++ xs) !! (n + 1))
+            Nothing -> Nothing
+-- 2
+module Cycle where
+
+import Data.List (elemIndex)
+
+data Direction = L | R deriving (Show)
+
+cycleList :: (Eq a) => Direction -> [a] -> a -> Maybe a
+cycleList d xs x = 
+            do
+              i <- elemIndex x xs
+              pure $ (xs ++ xs) !! (index d i xs)
+              where index L i xs = i - 1 + (length xs)
+                    index R i xs = i + 1
