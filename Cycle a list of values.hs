121d91b2ccea23c851addba50b5157fe20153735
Ran <1012869439@qq.com>
2019-01-08 14:51:41 +0800

    more elegant!

--- Cycle a list of values.hs
+++ Cycle a list of values.hs
@@ -28,3 +28,16 @@ cycleList d xs x =
               pure $ (xs ++ xs) !! (index d i xs)
               where index L i xs = i - 1 + (length xs)
                     index R i xs = i + 1
+-- 3
+module Cycle where
+
+import Data.List (elemIndex)
+
+data Direction = L | R deriving (Show)
+
+cycleList :: (Eq a) => Direction -> [a] -> a -> Maybe a
+cycleList d xs x = do 
+  i <- elemIndex x xs 
+  return $ xs !! (dir i `mod` length xs)
+  where dir = case d of L -> pred
+                        R -> succ
