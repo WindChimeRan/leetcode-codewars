487a375751c91849fe21319c5f6f5d4314800fc1
Ran <1012869439@qq.com>
2019-03-09 22:58:43 +0800

    can be better

--- 897. Increasing Order Search Tree.py
+++ 897. Increasing Order Search Tree.py
@@ -28,3 +28,30 @@ class Solution:
                 temp.right = TreeNode(result[i])
                 temp = temp.right
             return root
+
+# 2
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        
+        result = self.temp = TreeNode(-1)
+        
+        def trav(root: TreeNode):
+            if not root:
+                return
+            else:
+                trav(root.left)
+                root.left = None
+                self.temp.right = root
+                self.temp = self.temp.right
+                trav(root.right)
+
+        
+        trav(root)
+        return result.right
