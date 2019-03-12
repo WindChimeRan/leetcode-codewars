029827ac8bb058fa33c708dcac1a43ad2e81cebe
Ran <1012869439@qq.com>
2019-03-12 19:51:58 +0800

    double pointer

--- 925. Long Pressed Name.py
+++ 925. Long Pressed Name.py
@@ -30,3 +30,16 @@ class Solution:
                 return False
         else:
             return True
+# 2
+from itertools import zip_longest
+class Solution:
+    def isLongPressedName(self, name: str, typed: str) -> bool:
+        
+        i = 0
+        for j in range(len(typed)):
+
+            if i < len(name) and name[i] == typed[j]:
+                i += 1
+            elif j == 0 or typed[j] != typed[j-1]:
+                return False
+        return i == len(name)
