9dbc727c9177125bf313ee2fab901c5cb51f43ae
Ran <1012869439@qq.com>
2019-03-14 01:35:10 +0800

    interesting

--- Plus - minus - plus - plus - ... - Count.hs
+++ Plus - minus - plus - plus - ... - Count.hs
@@ -16,6 +16,13 @@ toBool xs = map (\x -> if x >= 0 then 1 else 0) xs
 helper :: [Int] -> Int
 helper xs = sum $ map (uncurry xor) $ zip xs (tail xs)
 
+-- 2
+module SignChange (count) where
+import Data.List
+count :: (Num a,Ord a) => [a] -> Int
+count [] = 0
+count xs = pred . length . group . map (>=0) $ xs
+
 -- others 
 module SignChange (count) where
 
