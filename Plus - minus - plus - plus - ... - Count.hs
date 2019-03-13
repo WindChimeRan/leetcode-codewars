c3bb1a600510db3a515d3932abe53dba0feaae24
Ran <1012869439@qq.com>
2019-03-14 01:25:36 +0800

    QAQ interesting

---
+++ Plus - minus - plus - plus - ... - Count.hs
@@ -0,0 +1,34 @@
+-- https://www.codewars.com/kata/plus-minus-plus-plus-dot-dot-dot-count/train/haskell
+
+-- 1
+module SignChange (count) where
+
+import Data.Bits (xor)
+
+count :: (Num a,Ord a) => [a] -> Int
+count []  = 0
+count [x] = 0
+count xs  = helper . toBool $ xs
+
+toBool :: (Num a,Ord a) => [a] -> [Int]
+toBool xs = map (\x -> if x >= 0 then 1 else 0) xs
+
+helper :: [Int] -> Int
+helper xs = sum $ map (uncurry xor) $ zip xs (tail xs)
+
+-- others 
+module SignChange (count) where
+
+import Data.Function (on)
+
+count :: (Num a, Ord a) => [a] -> Int
+count = length . filter id . (zipWith ((/=) `on` (>= 0)) <*> tail)
+
+-- others
+module SignChange (count) where
+
+import Data.List
+import Data.Function
+
+count :: (Num a, Ord a) => [a] -> Int
+count = max 0 . pred . length . groupBy ((==) `on` (>= 0))
