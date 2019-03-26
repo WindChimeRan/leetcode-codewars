a4dcdb1d31861aaaf89b89fc4ba593073880800a
Ran <1012869439@qq.com>
2019-03-26 20:19:49 +0800

    小伙伴的讲解QAQ

--- 134. Gas Station.py
+++ 134. Gas Station.py
@@ -1,3 +1,6 @@
+# 就是首先要找到可以让剩余油量大于0的加油站，然后假设这个加油站是起点继续往后走，
+# 如果能走到尽头（还有油量剩余）那么这时的剩余油量大于该点之前的所需油量就可以走完，如果走着走着发现油不够了，就从下一个加油站开始走
+#
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
 
