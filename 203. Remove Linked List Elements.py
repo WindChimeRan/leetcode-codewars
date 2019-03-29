66113d8f5f422c11b8cf4ed20240bcced7d9c562
Ran <1012869439@qq.com>
2019-03-29 23:45:56 +0800

    add while

--- 203. Remove Linked List Elements.py
+++ 203. Remove Linked List Elements.py
@@ -14,3 +14,24 @@ class Solution:
         else:
             head.next = self.removeElements(head.next, val)
             return head
+# 2
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        if head is None:
+            return None
+        elif head.val == val:
+            return self.removeElements(head.next, val)
+        else:
+            cur = head
+            while cur.next:
+                if cur.next.val == val:
+                    cur.next = cur.next.next
+                else:
+                    cur = cur.next
+            return head
