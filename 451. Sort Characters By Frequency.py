db13b2e2609353cd9263a5b2987460598f166891
Ran <1012869439@qq.com>
2019-07-07 17:01:03 +0800

    Update 451. Sort Characters By Frequency.py

--- 451. Sort Characters By Frequency.py
+++ 451. Sort Characters By Frequency.py
@@ -6,3 +6,8 @@ class Solution:
         for k, t in cnt:
             res.append(k*t)
         return ''.join(res)
+
+from collections import Counter
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        return ''.join(k*t for k, t in Counter(s).most_common())
