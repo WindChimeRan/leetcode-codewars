7fd0b96f65bc693b959b056ff950075327b0ff80
Ran <1012869439@qq.com>
2019-07-20 14:00:30 +0800

    Update 230. Kth Smallest Element in a BST.py

--- 230. Kth Smallest Element in a BST.py
+++ 230. Kth Smallest Element in a BST.py
@@ -16,3 +16,26 @@ class Solution:
                 return tolist(root.left) + [root.val] + tolist(root.right)
             
         return tolist(root)[k-1]
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        
+        
+        def tolist(root):
+            if not root:
+                return
+            else:
+                yield from tolist(root.left)
+                yield root.val
+                yield from tolist(root.right)
+            
+        for i, v in enumerate(tolist(root)):
+            if i == k-1:
+                return v
