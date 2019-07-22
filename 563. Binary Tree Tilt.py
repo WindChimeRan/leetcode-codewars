6c73336def585279aaba9a2c221cc4a3ba9d0d94
Ran <1012869439@qq.com>
2019-07-22 18:08:12 +0800

    Update 563. Binary Tree Tilt.py

--- 563. Binary Tree Tilt.py
+++ 563. Binary Tree Tilt.py
@@ -19,3 +19,25 @@ class Solution:
         else:
             return abs(sumTree(root.left) - sumTree(root.right)) + self.findTilt(root.left) + self.findTilt(root.right)
         
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def findTilt(self, root: TreeNode) -> int:
+        _sum = 0
+        def sumTree(root):
+            nonlocal _sum
+            if not root:
+                return 0
+            else:
+                left = sumTree(root.left)
+                right = sumTree(root.right)
+                _sum += abs(left-right)
+                return  left + root.val + right
+            
+        sumTree(root)
+        return _sum
+        
