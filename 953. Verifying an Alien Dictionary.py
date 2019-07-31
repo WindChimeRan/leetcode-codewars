e8c9770dc8d41e9a63be6cc7f6beadf3d3a7794e
Ran <1012869439@qq.com>
2019-07-31 16:26:12 +0800

    Create 953. Verifying an Alien Dictionary.py

---
+++ 953. Verifying an Alien Dictionary.py
@@ -0,0 +1,15 @@
+
+class Solution:
+    def isAlienSorted(self, words: 'List[str]', order: 'str') -> 'bool':
+        tab = {k: v for v, k in enumerate(order)}
+        def trans(word):
+            return tuple(map(lambda x: tab[x], word))
+        init = trans(words[0])
+        for w in words:
+            cur = trans(w)
+            if cur < init:
+                return False
+            else:
+                init = cur
+        return True
+        
