4b55e5fce356f614f4b8bbc4b38fdfdd94c8f34b
Ran <1012869439@qq.com>
2019-07-31 16:25:14 +0800

    Update 242. Valid Anagram.py

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -1,14 +1,4 @@
+from collections import Counter
 class Solution:
-    def isAlienSorted(self, words: 'List[str]', order: 'str') -> 'bool':
-        tab = {k: v for v, k in enumerate(order)}
-        def trans(word):
-            return tuple(map(lambda x: tab[x], word))
-        init = trans(words[0])
-        for w in words:
-            cur = trans(w)
-            if cur < init:
-                return False
-            else:
-                init = cur
-        return True
-        
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
