341f1a8bdd416a45f3965d96f90e0a8265eebf36
Ran <1012869439@qq.com>
2019-07-31 15:40:28 +0800

    callback

---
+++ 1078. Occurrences After Bigram.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findOcurrences(self, text: str, first: str, second: str) -> List[str]:
+        
+        def check_fst(tok):
+            if tok == first:
+                return None, check_snd
+            else:
+                return None, check_fst
+            
+        def check_snd(tok):
+
+            if tok == second:
+                return None, check_thd
+            elif tok == first:
+                return None, check_snd
+            else:
+                return None, check_fst
+            
+        def check_thd(tok):
+
+            if tok == first:
+                return tok, check_snd
+            else:
+                return tok, check_fst
+        
+        result = []
+        call_back = check_fst
+        for tok in text.split():
+            r, call_back = call_back(tok)
+            if r:
+                result.append(r)
+        return result
+            
+            
