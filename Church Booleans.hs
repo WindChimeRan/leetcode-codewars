56f3829d1a352a6a251489da3cef68b7ae2a69d6
Ran <1012869439@qq.com>
2019-12-07 23:11:36 -0600

    Create Church Booleans.hs

---
+++ Church Booleans.hs
@@ -0,0 +1,15 @@
+-- https://www.codewars.com/kata/church-booleans/train/haskell
+{-# Language RankNTypes #-}
+
+module Church (not,and,or,xor) where
+
+import Prelude hiding (Bool,False,True,not,and,or,(&&),(||),(==),(/=))
+import Church.Preloaded (Boolean,false,true)
+
+not :: Boolean -> Boolean
+and,or,xor :: Boolean -> Boolean -> Boolean
+
+not = \ a   -> a false true
+and = \ a b -> a b false
+or  = \ a b -> a true b
+xor = \ a b -> a (not b) b
